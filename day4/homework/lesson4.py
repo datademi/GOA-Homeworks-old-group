b3098ec580632ef46b14debdfb4edf2ea5d50ad3
datademi <mrisxaneazeso@gmail.com>
2024-03-01 08:07:28 -0800

    commit

---
+++ day4/homework/lesson4.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+time = int(input("how many years into the future do you want to travel: "))
+
+new_age = age + time
+
+print("when you travel in future and meet yourself you will be " + str(new_age))
---
+++ day4/homework/lesson4hw2.py
@@ -0,0 +1,5 @@
+number = int(input("enter the number you like: "))
+second_number = int(input("enter the number you dont like: "))
+
+number_second_number = number + second_number 
+print("we have to add it "+ str(number + second_number))
